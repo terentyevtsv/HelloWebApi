4c0d7362b925e33e229a32a42ead8772446c432c
Terentiev_SV <TerentievSV@samnipineft.ru>
2019-06-13 10:05:17 +0400

    Проект для WebApi

---
+++ HelloWebApi1/Controllers/UsersController.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HelloWebApi1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HelloWebApi1.Controllers
+{
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        private readonly UsersContext _context;
+
+        public UsersController(UsersContext context)
+        {
+            _context = context;
+            if (!_context.Users.Any())
+            {
+                _context.Users.Add(new User { Name = "Tom", Age = 26 });
+                _context.Users.Add(new User { Name = "Alice", Age = 31 });
+                _context.SaveChanges();
+            }
+        }
+
+        // GET: api/<controller>
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return _context.Users.ToList();
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            User user = _context.Users.FirstOrDefault(x => x.Id == id);
+            if (user == null)
+                return NotFound();
+            return new ObjectResult(user);
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public IActionResult Post([FromBody]User user)
+        {
+            if (user == null)
+            {
+                return BadRequest();
+            }
+
+            _context.Users.Add(user);
+            _context.SaveChanges();
+            return Ok(user);
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody]User user)
+        {
+            if (user == null)
+            {
+                return BadRequest();
+            }
+            if (!_context.Users.Any(x => x.Id == id))
+            {
+                return NotFound();
+            }
+
+            user.Id = id;
+
+            _context.Update(user);
+            _context.SaveChanges();
+            return Ok(user);
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            User user = _context.Users.FirstOrDefault(x => x.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            _context.Users.Remove(user);
+            _context.SaveChanges();
+            return Ok(user);
+        }
+    }
+}
--- HelloWebApi1/Startup.cs
+++ HelloWebApi1/Startup.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HelloWebApi1.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -25,6 +27,8 @@ namespace HelloWebApi1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string con = "Server=(localdb)\\mssqllocaldb;Database=usersdbstore;Trusted_Connection=True;MultipleActiveResultSets=true";
+            services.AddDbContext<UsersContext>(options => options.UseSqlServer(con));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
